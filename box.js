4b110c4dc55c41b4031cc95b707a5abc81a50183
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-07-11 11:17:17 -0400

    Update box.js

--- box.js
+++ box.js
@@ -19,7 +19,7 @@ class Box {
         push();
         World.remove(world, this.body);
         this.visibility = this.visibility - 5;
-        tint(255, 0);
+        tint(255, this.visibility);
         image(this.image, this.body.position.x, this.body.position.y, 20, 20);
         pop();
      }
