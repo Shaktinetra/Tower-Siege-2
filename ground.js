9bbde4b5a12a2899543c94fbb784588ed455dfaa
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-07-11 11:31:35 -0400

    Update ground.js

--- ground.js
+++ ground.js
@@ -3,7 +3,7 @@ class Ground {
       var options = {
           isStatic: true
       }
-
+      rectMode(CENTER);
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
