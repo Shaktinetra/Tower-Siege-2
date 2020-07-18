62a1df8742d1c35082bea707c40094c738d1ad58
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-07-18 16:42:27 -0400

    Update polygon.js

--- polygon.js
+++ polygon.js
@@ -13,7 +13,7 @@ class Polygon {
 
     display(){
         imageMode(CENTER);
-        image(this.image, this.body.position.x, this.body.position.y, 20, 20);
+        image(this.image, this.body.position.x, this.body.position.y, 30, 30);
         noFill();
     }
         }
